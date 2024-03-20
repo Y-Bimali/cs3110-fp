5d0c0439620a5cc6117852455276ad84e3d3ad9b
Desmond Ababio <dna35@cornell.edu>
2024-03-20 10:23:39 -0400

    added bisect and achieved 100% coverage for card

--- test/test_fp.ml
+++ test/test_fp.ml
@@ -34,14 +34,38 @@ let test_empty_card _ =
   assert_equal { rank = Zero; suit = Clubs } (empty_card Clubs);
   assert_equal { rank = Zero; suit = Hearts } (empty_card Hearts)
 
-let suite =
-  "test_suite"
+let test_empty_card_two _ =
+  let empty = empty_card Clubs in
+  assert_equal Clubs (suit_of empty);
+  assert_equal Zero (rank_of empty)
+
+let test_num_of _ =
+  assert_equal 0 (num_of { rank = Zero; suit = Spades });
+  assert_equal 1 (num_of { rank = Ace; suit = Hearts });
+  assert_equal 2 (num_of { rank = Two; suit = Hearts });
+  assert_equal 3 (num_of { rank = Three; suit = Clubs });
+  assert_equal 4 (num_of { rank = Four; suit = Spades });
+  assert_equal 5 (num_of { rank = Five; suit = Hearts });
+  assert_equal 6 (num_of { rank = Six; suit = Spades });
+  assert_equal 7 (num_of { rank = Seven; suit = Hearts });
+  assert_equal 8 (num_of { rank = Eight; suit = Hearts });
+  assert_equal 9 (num_of { rank = Nine; suit = Diamonds });
+  assert_equal 10 (num_of { rank = Ten; suit = Hearts });
+  assert_equal 11 (num_of { rank = Jack; suit = Hearts });
+  assert_equal 12 (num_of { rank = Queen; suit = Diamonds });
+  assert_equal 13 (num_of { rank = King; suit = Clubs })
+
+let tests =
+  "test_tests"
   >::: [
          "test_string_of_suit" >:: test_string_of_suit;
          "test_string_of_rank" >:: test_string_of_rank;
          "test_color_of" >:: test_color_of;
          "test_to_string" >:: test_to_string;
          "test_empty_card" >:: test_empty_card;
+         "test_num_of" >:: test_num_of;
+         "test_string_of_rank" >:: test_string_of_rank;
+         "test_empty_card_two" >:: test_empty_card_two;
        ]
 
-let () = run_test_tt_main suite
+let () = run_test_tt_main tests
