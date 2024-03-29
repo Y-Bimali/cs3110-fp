248a2d52c78a96f1fd080559d8a9c86214144137
Yinghui Bimali <yb272@cornell.edu>
2024-03-28 20:24:21 -0400

    Added instructions to demo in main.

--- bin/main.ml
+++ bin/main.ml
@@ -9,4 +9,19 @@ let rec game_loop g =
   | false -> ()
 
 let () = Random.self_init ()
-let _ = game_loop (new_game ())
+
+let instructions () =
+  print_endline "";
+  print_endline
+    "This is a demo of a Klondike Solitaire game. Supported commands are \
+     listed below. Commands are case insensitive.";
+  print_endline "Type \"New Game\" to start a new game.";
+  print_endline
+    "Type \"Draw\" or \"D\" to draw a card from the stock to the waste, or \
+     return all cards in the waste back to the stock. The next draw will flip \
+     cards from top to bottom again.";
+  print_endline "Type \"Quit\" to quit the program."
+
+let _ =
+  instructions ();
+  game_loop (new_game ())
