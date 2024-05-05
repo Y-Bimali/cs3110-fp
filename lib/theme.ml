f0b55ed4200ca66acc621792eac731bebbf5f45b
Yinghui Bimali <yb272@cornell.edu>
2024-05-05 13:13:10 -0400

    Restructure themes to non-module implementation
    - Current theme now stored in printer as a ref
    - Themes can only be retrieved from outside the module by [theme_of_string]
    - Adding a new theme only requires changing the txt files (theme and help), no code modification needed
    - No ability to add multiple names for a theme now

--- lib/theme.ml
+++ lib/theme.ml
@@ -1,14 +1,26 @@
 open ANSITerminal
 
-module type T = sig
-  val name : string
-  val bg : ANSITerminal.style
-  val red : ANSITerminal.style
-  val black : ANSITerminal.style
-  val faces : ANSITerminal.style
-  val backs : ANSITerminal.style
-  val back_pattern : ANSITerminal.style
-end
+type t = {
+  name : string;
+  bg : ANSITerminal.style;
+  red : ANSITerminal.style;
+  black : ANSITerminal.style;
+  faces : ANSITerminal.style;
+  backs : ANSITerminal.style;
+  back_pattern : ANSITerminal.style;
+}
+
+exception UnknownTheme
+
+let theme_list = ref []
+
+let rec find_theme str lst =
+  match lst with
+  | [] -> raise UnknownTheme
+  | h :: t ->
+      if str = String.lowercase_ascii h.name then h else find_theme str t
+
+let theme_of_string str = find_theme str !theme_list
 
 let color_parser foreground name =
   let name = String.lowercase_ascii name in
@@ -25,55 +37,28 @@ let color_parser foreground name =
     | _ -> failwith "Improperly formatted or spelled input color theme")
     (Bool.to_int foreground)
 
+let make data =
+  theme_list :=
+    {
+      name = List.nth data 0;
+      bg = color_parser false (List.nth data 1);
+      red = color_parser true (List.nth data 2);
+      black = color_parser true (List.nth data 3);
+      faces = color_parser false (List.nth data 4);
+      backs = color_parser true (List.nth data 5);
+      back_pattern = color_parser false (List.nth data 6);
+    }
+    :: !theme_list
+
+(** Add variables for new themes below, with the int argument for List.nth being
+    the corresponding line of the themes.txt file. The program will fail if that
+    line in themes.txt is badly formatted or unusable.*)
+
 let data_in =
   List.map (String.split_on_char ',')
     (BatList.of_enum (BatFile.lines_of "data/themes.txt"))
 
-module Make (S : sig
-  val data : string list
-end) =
-struct
-  let name = List.nth S.data 0
-  let bg = color_parser false (List.nth S.data 1)
-  let red = color_parser true (List.nth S.data 2)
-  let black = color_parser true (List.nth S.data 3)
-  let faces = color_parser false (List.nth S.data 4)
-  let backs = color_parser true (List.nth S.data 5)
-  let back_pattern = color_parser false (List.nth S.data 6)
-end
-
-(** Add the module for new themes below, with the int argument for List.nth
-    being the corresponding line of the themes.txt file. By convention, place a
-    capital M in fron to distinguish it from the variant name. *)
-
-module MClassic : T = Make (struct
-  let data = List.nth data_in 0
-end)
-
-module MSpaceship : T = Make (struct
-  let data = List.nth data_in 1
-end)
-
-module MUnderTheSea : T = Make (struct
-  let data = List.nth data_in 2
-end)
-
-(** Add the corresponding variant name here, and fix all code pattern matching
-    to this type.*)
-
-type t =
-  | Classic
-  | Spaceship
-  | UnderTheSea
-
-exception UnknownTheme
-
-(** Add a string name for the theme here to help functions decoding strings to
-    themes. You may add multiple names for the same theme if you find it
-    sensible. *)
-let theme_of_string str =
-  match String.lowercase_ascii str with
-  | "classic" -> Classic
-  | "spaceship" -> Spaceship
-  | "under the sea" -> UnderTheSea
-  | _ -> raise UnknownTheme
+let () =
+  for i = 0 to List.length data_in - 1 do
+    make (List.nth data_in i)
+  done
